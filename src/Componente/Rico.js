f337b803a15a3874d877ffa8feebcf04c9fda7ec
407 <407@academicos.cesde.edu.co>
2022-09-27 17:35:36 -0500

    se crea React Proyect

---
+++ src/Componente/Rico.js
@@ -0,0 +1,12 @@
+export function Rico(){
+
+    let nombre = "AlexElCapoFan"
+    //Interpolar significa Controlar la GUI desde JS
+    return(
+        <>   
+            <h1>Hola react, SOY: {nombre} </h1>
+            <h1> agua agua limonada tampico agua agua limonada tamPICOOOO</h1>
+            <hr/>
+        </>
+    )
+}
